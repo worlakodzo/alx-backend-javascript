11f39b84babfeee4921b533ba0c3284f88565aff
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-07-30 22:12:21 +0000

    Added 4-update_grade_by_city

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
 const createInt8TypedArray = (length, position, value) => {
-    const buffer = new ArrayBuffer(length);
-    const view = new Int8Array(buffer);
-    try {
-        view[position] = value;
-    } catch (error) {
-        throw new Error('Position outside range');
-    }
-    return view;
-}
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  try {
+    view[position] = value;
+  } catch (error) {
+    throw new Error('Position outside range');
+  }
+  return view;
+};
 
 export default createInt8TypedArray;
