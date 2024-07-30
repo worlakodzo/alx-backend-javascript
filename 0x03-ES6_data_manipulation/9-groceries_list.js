6f5a01ee1efae9451694fcee74b0c31c3a60de98
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-07-30 23:27:06 +0000

    Added 9-groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+function groceriesList() {
+  const groceries = new Map();
+  groceries.Apples = 10;
+  groceries.Tomatoes = 10;
+  groceries.Pasta = 1;
+  groceries.Rice = 1;
+  groceries.Banana = 5;
+  return groceries;
+}
+
+export default groceriesList;
