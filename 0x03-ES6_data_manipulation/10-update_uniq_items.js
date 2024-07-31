a56d978ff5aa8d5ba54ba745e66e68f1d311e82c
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-07-31 00:19:48 +0000

    Added 10-update_uniq_item

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,8 +3,6 @@ const updateUniqueItems = (map) => {
     for (const [key, value] of map) {
       if (value === 1) {
         map.set(key, 100);
-      } else {
-        throw new Error('Cannot process');
       }
     }
     return map;
